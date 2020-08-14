576c6341d3772633eabdb612075aeb0f0f888fbb
bc.han <crines85@gmail.com>
2020-08-14 15:28:05 +0900

    yes

--- main.js
+++ main.js
@@ -6,77 +6,56 @@
     var start = document.getElementById('start');
     var stop = document.getElementById('stop');
 
-  
     var startTime;
 
-    
     var elapsedTime = 0;
 
-    
     var timerId;
 
-    
     var timeToadd = 0;
 
 
-  
     function updateTimetText(){
 
-        
         var m = Math.floor(elapsedTime / 60000);
 
-       
         var s = Math.floor(elapsedTime % 60000 / 1000);
 
-        
         var ms = elapsedTime % 1000;
 
 
-       
-        m = ('0' + m).slice(-2); 
+        m = ('0' + m).slice(-2);
         s = ('0' + s).slice(-2);
         ms = ('0' + ms).slice(-3);
 
-        
         timer.textContent = m + ':' + s + ':' + ms;
     }
 
 
-   
     function countUp(){
 
-        
         timerId = setTimeout(function(){
 
-         
             elapsedTime = Date.now() - startTime + timeToadd;
             updateTimetText()
 
-           
             countUp();
 
-       
         },10);
     }
 
-   
+
     start.addEventListener('click',function(){
 
-        
         startTime = Date.now();
 
-       
         countUp();
     });
 
-    
     stop.addEventListener('click',function(){
 
-      
        clearTimeout(timerId);
 
-
-    
        timeToadd += Date.now() - startTime;
     });
 
